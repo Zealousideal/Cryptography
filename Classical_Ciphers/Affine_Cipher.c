b2fd783cb84e8e1eebf3ad648b8413b072681d7c
Zealousideal <desinoy2018@gmail.com>
2024-03-06 12:25:44 +0530

    Decryption of Affine Cipher

--- Classical_Ciphers/Affine_Cipher.c
+++ Classical_Ciphers/Affine_Cipher.c
@@ -33,6 +33,55 @@ void encrypt_affine(char *plaintext, int key_a, int key_b)
     }
     printf("\n");
 }
+
+void decrypt_affine(char *ciphertext, int key_a, int key_b)
+{
+    int key_a_inv = 0, i = 0, plaintext_val;
+    char plaintext;
+
+    for (int j = 0; j < 26; j++)
+        if (key_a != 0 && (key_a * j) % 26 == 1)
+        {
+            key_a_inv = j;
+            break;
+        }
+
+    if (key_a_inv == 0)
+    {
+        printf("Invalid key_a. Modular multiplicative inverse does not exist.\n");
+        return;
+    }
+
+    while (ciphertext[i] != '\0' && (strlen(ciphertext) - 1) > i)
+    {
+        if (ciphertext[i] != ' ')
+        {
+            if (islower(ciphertext[i]))
+            {
+                plaintext_val = ((((int)ciphertext[i] - 97 - key_b) * key_a_inv) % 26);
+                if (plaintext_val < 0)
+                    plaintext_val += 26;
+                plaintext = (char)(plaintext_val + 97);
+            }
+
+            else if (isupper(ciphertext[i]))
+            {
+                plaintext_val = ((((int)ciphertext[i] - 65 - key_b) * key_a_inv) % 26);
+                if (plaintext_val < 0)
+                    plaintext_val += 26;
+                plaintext = (char)(plaintext_val + 65);
+            }
+
+            printf("%c", plaintext);
+        }
+
+        else
+            printf(" ");
+        i++;
+    }
+    printf("\n");
+}
+
 int main()
 {
     int key_a = 17, key_b = 20;
@@ -44,5 +93,11 @@ int main()
 
     printf("The encrypted message is: \n");
     encrypt_affine(plaintext, key_a, key_b);
+
+    printf("Enter the ciphertext:\n");
+    fgets(ciphertext, sizeof(ciphertext), stdin);
+
+    printf("The plaintext is: \n");
+    decrypt_affine(ciphertext, key_a, key_b);
     return 0;
 }
